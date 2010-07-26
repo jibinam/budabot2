51c02381e7fe9a803e1f1e69982bb4eb071c1aad
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-07-26 05:03:35 +0000

    updated the Command queries to use the new cmdcfg schema

--- branches/0.7/core/Command.class.php
+++ branches/0.7/core/Command.class.php
@@ -5,7 +5,7 @@ class Command {
 /*===============================
 ** Name: Command
 ** 	Register a command
-*/	public static function register_command($type, $module, $filename, $command, $access_level = ALL, $description = '') {
+*/	public static function register($type, $module, $filename, $command, $access_level, $description = '', $is_core = false) {
 		global $db;
 
 		if (!Command::processCommandArgs($type, $access_level)) {
@@ -15,18 +15,57 @@ class Command {
 
 		$command = strtolower($command);
 		$description = str_replace("'", "''", $description);
+		$is_core = $is_core ? 1 : 0;
 
-		for ($i = 0; $i < count($type); $i++) {
-			if (Settings::get('debug') > 1) print("Adding Command to list:($command) File:($filename)\n");
-			if (Settings::get('debug') > 1) print("                 Admin:({$access_level[$i]}) Type:({$type[$i]})\n");
-			if (Settings::get('debug') > 2) sleep(1);
-			
-			if (Command::find_command($command) != false) {
-				$db->query("UPDATE cmdcfg_<myname> SET `module` = '$module', `verify` = 1, `file` = '$filename', `description` = '$description' WHERE `cmd` = '$command' AND `type` = '{$type[$i]}'");
-			} else {
-				$db->query("INSERT INTO cmdcfg_<myname> (`module`, `type`, `file`, `cmd`, `access_level`, `description`, `verify`, `cmdevent`, `status`) VALUES ('$module', '{$type[$i]}', '$filename', '$command', $access_level, '$description', 1, 'cmd', '".Settings::get("default module status")."')");
-			}
+		if (Settings::get('debug') > 1) print("Adding Command to list:($command) File:($filename)\n");
+		if (Settings::get('debug') > 2) sleep(1);
+
+		if (Command::find_command($command) != false) {
+			$sql = "
+				UPDATE
+					cmdcfg_<myname>
+				SET
+					`module` = '$module',
+					`regex` = '',
+					`file` = '$filename',
+					`is_core` = $is_core,
+					`description` = '$description',
+					`verify` = 1
+				WHERE
+					`cmd` = '$command'";
+		} else {
+			$sql = "
+				INSERT INTO cmdcfg_<myname> (
+					`module`,
+					`regex`,
+					`file`,
+					`is_core`,
+					`cmd`,
+					`tell_status`,
+					`tell_access_level`,
+					`guild_status`,
+					`guild_access_level`,
+					`priv_status`,
+					`priv_access_level`,
+					`description`,
+					`verify`
+				) VALUES (
+					'$module',
+					'',
+					'$filename',
+					$is_core,
+					'$command'
+					". Settings::get("default module status") .",
+					{$access_level['tell']},
+					". Settings::get("default module status") .",
+					{$access_level['guild']},
+					". Settings::get("default module status") .",
+					{$access_level['priv']},
+					'$description',
+					1
+				)";
 		}
+		$db->query($sql);
 	}
 	
 	public static function find_command($name, $type = null) {
