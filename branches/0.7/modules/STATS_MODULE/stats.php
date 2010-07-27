937ae1b152dc5f98d3d2d7ffb3965e583aecdd27
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-07-27 07:15:14 +0000

    added number of fighting opponents to !stats

--- branches/0.7/modules/STATS_MODULE/stats.php
+++ branches/0.7/modules/STATS_MODULE/stats.php
@@ -124,7 +124,8 @@ if (preg_match("/^stats$/i", $message, $arr)) {
 		<a href=skillid://676>Profession Duel Kills</a>
 		<a href=skillid://677>Profession Duel Deaths</a>
 		<a href=skillid://679>Solo Deaths</a>
-		<a href=skillid://681>Team Deaths</a>";
+		<a href=skillid://681>Team Deaths</a>
+		<a href=skillid://410>Number of fighting opponents</a>";
 
 	$msg = Text::makeBlob('Stats', $window);
 
