8bbf91158cb28dc2bdb0f0971da6f2c3aafcd9cd
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-07-30 04:39:42 +0000

    added syntax_error fix to GUILD_MANAGEMENT_MODULE

--- branches/0.7/modules/GUILD_MANAGEMENT_MODULE/GUILD_MANAGEMENT_MODULE.php
+++ branches/0.7/modules/GUILD_MANAGEMENT_MODULE/GUILD_MANAGEMENT_MODULE.php
@@ -2,7 +2,7 @@
 	$MODULE_NAME = "GUILD_MANAGEMENT_MODULE";
 
 	//Check Inactives
-	Command::register("msg", $MODULE_NAME, "inactive_mem.php", "inactivemem", ADMIN, "Check for inactive members");
+	Command::register("", $MODULE_NAME, "inactive_mem.php", "inactivemem", ADMIN, "Check for inactive members");
 
 	//Help files
     Help::register("inactivemem", $MODULE_NAME, "manage_guild.txt", ADMIN, "Help on Checking for Inactive Members");
