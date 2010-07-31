06cacfa014019cbbabcb23f469b581fea5f224de
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-07-31 01:07:40 +0000

    moved !cmdlist to SYSTEM core module and added !eventlist

--- branches/0.7/core/CONFIG/CONFIG.php
+++ branches/0.7/core/CONFIG/CONFIG.php
@@ -4,7 +4,6 @@
 	//Commands
 	Command::register("", $MODULE_NAME, "cmdcfg.php", "config", MODERATOR);
 	Command::register("", $MODULE_NAME, "searchcmd.php", "searchcmd", MODERATOR);
-	Command::register("", $MODULE_NAME, "cmdlist.php", "cmdlist", MODERATOR);
 
 	//Help Files
 	Help::register("config", $MODULE_NAME, "config.txt", MODERATOR, "Configure Commands/Events of the Bot.");
--- branches/0.7/core/SYSTEM/SYSTEM.php
+++ branches/0.7/core/SYSTEM/SYSTEM.php
@@ -10,6 +10,8 @@
 	Command::register("", $MODULE_NAME, "shutdown.php", "shutdown", ADMIN);
 	Command::register("", $MODULE_NAME, "uptime.php", "uptime", MEMBER);
 	Command::register("", $MODULE_NAME, "memory.php", "memory", MEMBER);
+	Command::register("", $MODULE_NAME, "cmdlist.php", "cmdlist", MEMBER);
+	Command::register("", $MODULE_NAME, "eventlist.php", "eventlist", MEMBER);
 
 	//Help Files
 	Help::register("systemhelp", $MODULE_NAME, "system.txt", ADMIN, "Admin System Help file.");
