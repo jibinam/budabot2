d8791dd36d92ce314b331483174bd2c91a836ac8
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-08-12 01:28:15 +0000

    added descriptions for logon/logoff events that support !is and !orglist commands

--- branches/0.7/modules/ORGLIST_MODULE/ORGLIST_MODULE.php
+++ branches/0.7/modules/ORGLIST_MODULE/ORGLIST_MODULE.php
@@ -4,13 +4,13 @@
 	// Checks who in an org is online
 	Command::register($MODULE_NAME, "orglist.php", "orglist", MODERATOR, "Check someones org roster");
 	Command::register($MODULE_NAME, "orglist.php", "onlineorg", MODERATOR, "Check someones org roster");
-	Event::register("logOn", $MODULE_NAME, "orglist.php", "orglist");
-	Event::register("logOff", $MODULE_NAME, "orglist.php", "orglist");
+	Event::register("logOn", $MODULE_NAME, "orglist.php", "Gets online status of org members");
+	Event::register("logOff", $MODULE_NAME, "orglist.php", "Gets offline status of org members");
 	
 	// Checks if a player is online
 	Command::register($MODULE_NAME, "is_online.php", "is", ALL, "Checks if a player is online");
-	Event::register("logOn", $MODULE_NAME, "is_online.php", "is");
-	Event::register("logOff", $MODULE_NAME, "is_online.php", "is");
+	Event::register("logOn", $MODULE_NAME, "is_online.php", "Gets online status of player");
+	Event::register("logOff", $MODULE_NAME, "is_online.php", "Gets offline status of player");
 
 	// Help files
 	Help::register("orglist", $MODULE_NAME, "orglist.txt", MODERATOR, "See who is online from someones org.");
