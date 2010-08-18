7a9a13ac978ce14f87ad97ce5064439df18fe386
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-08-18 16:07:01 +0000

    code cleanup

--- branches/0.7/modules/INFO_MODULE/info.php
+++ branches/0.7/modules/INFO_MODULE/info.php
@@ -12,56 +12,55 @@
    ** Copyright (C) 2007 Jason Wheeler
    */
 
-if (preg_match("/^info(.*)$/i", $message)) {
-	$path = getcwd() . "/modules/INFO_MODULE/info/";
-	$fileExt = ".txt";
-	$msg = "";
-	
-	// if they want the list of topics
-	if (preg_match("/^info$/i", $message)) {
-		if ($handle = opendir($path)) {
-			$topicList = array();
 
-		    /* This is the correct way to loop over the directory. */
-		    while (false !== ($fileName = readdir($handle))) {
-			    // if file has the correct extension, it's a topic file
-		        if (strpos($fileName, $fileExt)) {
-        			$topicList[] =  str_replace($fileExt, '', $fileName);
-		        }
-		    }
+$path = getcwd() . "/modules/INFO_MODULE/info/";
+$fileExt = ".txt";
+$msg = "";
 
-		    closedir($handle);
-		    
-		    global $vars;
-		    global $settings;
-		    $linkContents = '';
-		    forEach ($topicList as $topic) {
-				$linkContents .= Text::makeLink($topic, "/tell " . $vars['name'] . " " . $settings['symbol'] . "info $topic", 'chatcmd') . "\n";  
-		    }
-		    
-		    if ($linkContents) {
-				$msg = Text::makeBlob('Topics (' . count($topicList) . ')', $linkContents);
-		    } else {
-			 	$msg = "No topics available.";   
-		    }
-		} else {
-			$msg = "Error reading topics.";	
+// if they want the list of topics
+if (preg_match("/^info$/i", $message)) {
+	if ($handle = opendir($path)) {
+		$topicList = array();
+
+		/* This is the correct way to loop over the directory. */
+		while (false !== ($fileName = readdir($handle))) {
+			// if file has the correct extension, it's a topic file
+			if (strpos($fileName, $fileExt)) {
+				$topicList[] =  str_replace($fileExt, '', $fileName);
+			}
 		}
-	} else if (preg_match("/^info (.*)$/i", $message, $arr)) {
-		// if they want a certain topic
-		// get the filename and read in the file
-		$fileName = $arr[1];
-		$info = getTopicContents($path, $fileName, $fileExt);
+
+		closedir($handle);
 		
-		// make sure the $ql is an integer between 1 and 300
-		if (!$info) {
-			$msg = "No info for $fileName could be found";
-		} else {	
-			$msg = Text::makeBlob($fileName, $info);
+		global $vars;
+		global $settings;
+		$linkContents = '';
+		forEach ($topicList as $topic) {
+			$linkContents .= Text::makeLink($topic, "/tell " . $vars['name'] . " " . $settings['symbol'] . "info $topic", 'chatcmd') . "\n";  
 		}
+		
+		if ($linkContents) {
+			$msg = Text::makeBlob('Topics (' . count($topicList) . ')', $linkContents);
+		} else {
+			$msg = "No topics available.";   
+		}
+	} else {
+		$msg = "Error reading topics.";	
 	}
+} else if (preg_match("/^info (.*)$/i", $message, $arr)) {
+	// if they want a certain topic
+	// get the filename and read in the file
+	$fileName = $arr[1];
+	$info = getTopicContents($path, $fileName, $fileExt);
 	
-	$this->send($msg, $sendto);
+	// make sure the $ql is an integer between 1 and 300
+	if (!$info) {
+		$msg = "No info for $fileName could be found";
+	} else {	
+		$msg = Text::makeBlob($fileName, $info);
+	}
 }
+
+$this->send($msg, $sendto);
 
 ?>
