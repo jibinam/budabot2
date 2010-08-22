df2c364da5cfd35baf14f99759c5c145c3946548
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-08-22 08:43:03 +0000

    renamed created a config template file; if config file doesn't exist, one will be created from the config template file

--- branches/0.7/main.php
+++ branches/0.7/main.php
@@ -67,6 +67,9 @@ if (isWindows()) {
 
 //Load Required Files
 $config_file = $argv[1];
+if (!file_exists($config_file)) {
+	copy('config.template.php', $config_file) or die("could not create config file: $config_file");
+}
 require_once $config_file;
 require_once './core/Budabot.class.php';
 
