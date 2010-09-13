52ffdab7b1ccb30a7dd612be69f5cdb8f00435d1
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-09-13 21:19:16 +0000

    merged DYNA_MODULE into EXTRAHELPBOT_MODULE

--- branches/2.0/modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
+++ branches/2.0/modules/EXTRAHELPBOT_MODULE/EXTRAHELPBOT_MODULE.php
@@ -2,10 +2,15 @@
 	require_once 'db_utils.php';
 
 	$MODULE_NAME = "EXTRAHELPBOT_MODULE";
+	
+	DB::loadSQLFile($MODULE_NAME, "dyna");
  
 	Command::register($MODULE_NAME, "mobloot.php", "mobloot", ALL, "loot QL Infos ");
 	Command::register($MODULE_NAME, "random.php", "random", ALL, "Random order");
 	Command::register($MODULE_NAME, "cluster.php", "cluster", ALL, "cluster location");
 	Command::register($MODULE_NAME, "buffitem.php", "buffitem", ALL, "buffitem look up");
 	Command::register($MODULE_NAME, "whatbuffs.php", "whatbuffs", ALL, "find items that buff");
+	Command::register($MODULE_NAME, "dyna.php", "dyna", ALL, "Search for RK Dynaboss");
+	
+	Help::register($MODULE_NAME, "dyna.txt", "dyna", ALL, "Search for RK Dynaboss");
 ?>
