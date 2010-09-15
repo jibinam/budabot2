163d50e16db557d953f052b20d1e70508eceb6fb
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-09-15 19:33:12 +0000

    merged EXTRAHELPBOT_MODULE functionality into HELPBOT_MODULE

--- branches/2.0/modules/HELPBOT_MODULE/HELPBOT_MODULE.php
+++ branches/2.0/modules/HELPBOT_MODULE/HELPBOT_MODULE.php
@@ -1,54 +1,47 @@
 <?php
+	require_once 'db_utils.php';
+	require_once 'trickle_functions.php';
+
 	$MODULE_NAME = "HELPBOT_MODULE";
-
-	//KOS-List Part
-	Command::register($MODULE_NAME, "kos_list.php", "kos", ALL, "Shows the Kill On Sight List");
-
-	//Time Part
-	Command::register($MODULE_NAME, "time.php", "time", ALL, "Shows the time in the different timezones");
-
-    //Whois Part
-	Command::register($MODULE_NAME, "whois.php", "whois", ALL, "Char Infos (only current dim)");
-
-    //Whoisall Part
-	Command::register($MODULE_NAME, "whois.php", "whoisall", ALL, "Char Infos (all dim)");
-
-	//Whoisorg Part
-	Command::register($MODULE_NAME, "whois.php", "whoisorg", ALL, "Org Infos");
-
-	//Biomaterial identification
-	Command::register($MODULE_NAME, "biomat_identify.php", "bio", ALL, "Biomaterial Identify");
 	
-	//Calculator
+	Event::register("setup", $MODULE_NAME, "setup.php");
+	
+	DB::loadSQLFile($MODULE_NAME, "dyna");
+	DB::loadSQLFile($MODULE_NAME, "research");
+	DB::loadSQLFile($MODULE_NAME, "trickle");
+
+	Command::register($MODULE_NAME, "kos_list.php", "kos", ALL, "Shows the Kill On Sight List");
+	Command::register($MODULE_NAME, "time.php", "time", ALL, "Shows the time in the different timezones");
+	Command::register($MODULE_NAME, "whois.php", "whois", ALL, "Char Infos (only current dim)");
+	Command::register($MODULE_NAME, "whois.php", "whoisall", ALL, "Char Infos (all dim)");
+	Command::register($MODULE_NAME, "whois.php", "whoisorg", ALL, "Org Infos");
+	Command::register($MODULE_NAME, "biomat_identify.php", "bio", ALL, "Biomaterial Identify");
 	Command::register($MODULE_NAME, "calc.php", "calc", ALL, "Calculator");
-
-	//OE Calculator
 	Command::register($MODULE_NAME, "oe.php", "oe", ALL, "OE");
+	Command::register($MODULE_NAME, "player_history.php", "history", ALL, "Show a history of a player");
+	Command::register($MODULE_NAME, "smileys.php", "smileys", ALL, "The meaning of different Smileys");
+	Command::register($MODULE_NAME, "inspect.php", "inspect", ALL, "Inspects Christmas/Eart Gifts and Peren. Containers");
+	Command::register($MODULE_NAME, "aigen.php", "aigen", ALL, "Info about Alien City Generals(which VBs they drop)");
+	Command::register($MODULE_NAME, "aiarmor.php", "aiarmor", ALL, "Tradeskillprocess for Alien Armor");
+	Command::register($MODULE_NAME, "trickle.php", "trickle", ALL, "Shows how much skills you will gain by increasing an ability");
+	Command::register($MODULE_NAME, "mobloot.php", "mobloot", ALL, "loot QL Infos ");
+	Command::register($MODULE_NAME, "random.php", "random", ALL, "Random order");
+	Command::register($MODULE_NAME, "cluster.php", "cluster", ALL, "cluster location");
+	Command::register($MODULE_NAME, "buffitem.php", "buffitem", ALL, "buffitem look up");
+	Command::register($MODULE_NAME, "whatbuffs.php", "whatbuffs", ALL, "find items that buff");
+	Command::register($MODULE_NAME, "dyna.php", "dyna", ALL, "Search for RK Dynaboss");
+	Command::register($MODULE_NAME, "research.php", "research", ALL, "Info on Research");
 
-	//Flip or Roll command
+	// Flip or Roll command
 	Command::register($MODULE_NAME, "roll.php", "flip", ALL, "Flip a coin");
 	Command::register($MODULE_NAME, "roll.php", "roll", ALL, "Roll a random number");
 	Command::register($MODULE_NAME, "roll.php", "verify", ALL, "Verifies a flip/roll");
+	
+	// Max XP calculator
+	Command::register($MODULE_NAME, "cap_xp.php", "capsk", ALL, "Max SK Calculator");
+	Command::register($MODULE_NAME, "cap_xp.php", "capxp", ALL, "Max XP Calculator");
 
-	//Player History
-	Command::register($MODULE_NAME, "player_history.php", "history", ALL, "Show a history of a player");
-	
-	//Smileys
-	Command::register($MODULE_NAME, "smileys.php", "smileys", ALL, "The meaning of different Smileys");
-	
-	//Inspect
-	Command::register($MODULE_NAME, "inspect.php", "inspect", ALL, "Inspects Christmas/Eart Gifts and Peren. Containers");
-	
-	//Alien City Generals
-	Command::register($MODULE_NAME, "aigen.php", "aigen", ALL, "Info about Alien City Generals(which VBs they drop)");
-	
-	//Alien Armor
-	Command::register($MODULE_NAME, "aiarmor.php", "aiarmor", ALL, "Tradeskillprocess for Alien Armor");
-
-	//Setup
-	Event::register("setup", $MODULE_NAME, "setup.php");
-
-	//Help files
+	// Help files
 	Help::register($MODULE_NAME, "whois.txt", "whois", ALL, "Show char stats at current and all dimensions");
     Help::register($MODULE_NAME, "biomat.txt", "biomat", ALL, "Identify an Biomaterial");
     Help::register($MODULE_NAME, "calculator.txt", "calculator", ALL, "Calculator");
@@ -60,4 +53,12 @@
     Help::register($MODULE_NAME, "smiley_title_inspect.txt", "smiley_title_inspect", ALL, "Help for Smiley,Title Level and Inspect");
     Help::register($MODULE_NAME, "aiarmor.txt", "aiarmor", ALL, "Alien armor Tradeskillprocess");
 	Help::register($MODULE_NAME, "alien_generals.txt", "alien_generals", ALL, "Alien City Generals Info");
+	Help::register($MODULE_NAME, "buffitem.txt", "buffitem", ALL, "How to use buffitem");
+	Help::register($MODULE_NAME, "cluster.txt", "cluster", ALL, "How to use cluster");
+	Help::register($MODULE_NAME, "mobloot.txt", "mobloot", ALL, "How to use mobloot");
+	Help::register($MODULE_NAME, "whatbuffs.txt", "whatbuffs", ALL, "How to use whatbuffs");
+	Help::register($MODULE_NAME, "dyna.txt", "dyna", ALL, "Search for RK Dynaboss");
+	Help::register($MODULE_NAME, "research.txt", "Research", ALL, "Info on Research");
+	Help::register($MODULE_NAME, "capxp.txt", "capxp", ALL, "Set your reasearch bar for max xp/sk");
+	Help::register($MODULE_NAME, "trickle.txt", "Trickle", ALL, "How to use trickle");
 ?>
