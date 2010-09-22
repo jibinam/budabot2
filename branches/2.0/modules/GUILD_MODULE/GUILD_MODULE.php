9a53b53852df9e6db5c67cf603483c0e7f072ca7
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2010-09-22 22:37:42 +0000

    merged GUILD_MANAGEMENT_MODULE functionality into GUILD_MODULE

--- branches/2.0/modules/GUILD_MODULE/GUILD_MODULE.php
+++ branches/2.0/modules/GUILD_MODULE/GUILD_MODULE.php
@@ -9,9 +9,11 @@
 	Event::register("orgmsg", $MODULE_NAME, "notify_auto.php", 'Add or remove user if they join or leave or are kicked from org', 1);
 
 	Command::register($MODULE_NAME, "notify.php", "notify", MODERATOR, "Adding a member man. to the notify list", 1);
+	Command::register($MODULE_NAME, "inactive_mem.php", "inactivemem", ADMIN, "Check for inactive members");
 
 	Settings::add("bot_notify", $MODULE_NAME, "Show/Hide Logoffs in Org Chat (Spam Prevention)", "edit", "1", "Show Logoffs;Hide Logoffs", '1;0', MODERATOR, "botnotify.txt", 1);
 	
 	//Helpfile
     Help::register($MODULE_NAME, "notify.txt", "notify", MODERATOR, "Add or remove a player from the notify list.");
+	Help::register($MODULE_NAME, "manage_guild.txt", "inactivemem", ADMIN, "Help on Checking for Inactive Members");
 ?>
