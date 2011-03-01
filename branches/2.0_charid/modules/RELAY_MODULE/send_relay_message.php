f96f2796b75c39049cdcd8b582c2cb0be6da7528
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2011-03-01 00:28:53 +0000

    created a separate branch for charid-based

---
+++ branches/2.0_charid/modules/RELAY_MODULE/send_relay_message.php
@@ -0,0 +1,23 @@
+<?php
+
+if (($chatBot->settings["relaybot"] != "Off") && ($chatBot->settings["bot_relay_commands"] == 1 || $args[2][0] != $chatBot->settings["symbol"])) {
+	$relayMessage = '';
+	if ($chatBot->settings['relaysymbol'] == 'Always relay') {
+		$relayMessage = $message;
+	} else if ($args[2][0] == $chatBot->settings['relaysymbol']) {
+		$relayMessage = substr($args[2], 1);
+	}
+
+	if ($relayMessage != '') {
+		$sender_link = Text::make_link($sender, $sender, "user");
+		$guild = $chatBot->vars["my guild"];
+		if ($type == "guild") {
+			$msg = "grc <grey>[{$guild}] {$sender_link}: ".$relayMessage."</font>";
+		} else if ($type == "priv") {
+			$msg = "grc <grey>[{$guild}] [Guest] {$sender_link}: ".$relayMessage."</font>";
+		}
+        send_message_to_relay($msg);
+	}
+}
+
+?>
