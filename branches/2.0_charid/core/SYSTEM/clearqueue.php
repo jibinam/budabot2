f36b0bf41f1f100101f757ef7057659fdf3a8c6d
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2011-03-01 00:31:45 +0000

    deleting 2.0_charid

--- branches/2.0_charid/core/SYSTEM/clearqueue.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (preg_match("/^clearqueue$/i", $message)) {
-	$chatBot->chatqueue->queue = array();
-
-	$chatBot->send('Chat queue has been cleared.', $sendto);
-}
-
-?>
--- branches/2.0_charid/modules/PRIVATE_CHANNEL_MODULE/record_priv_leave.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if ($type == "leavePriv") {
-	$db->exec("DELETE FROM online WHERE `name` = '$sender' AND `channel_type` = 'priv' AND added_by = '<myname>'");
-}
-
-?>
