cfb9a1cf00aada9a7910cb3a60e509a6dd7e4426
bigwheels16 <bigwheels16@7573f3da-27b8-11df-a76c-adfa62dee608>
2012-01-30 08:27:31 +0000

    added a few tweaks and fixes; added ScalaEntryPoint.java for making a runnable jar

---
+++ projects/porkmirror/src/main/java/com/jkbff/ao/playerinfoscraper/ScalaEntryPoint.java
@@ -0,0 +1,7 @@
+package com.jkbff.ao.playerinfoscraper;
+
+public class ScalaEntryPoint {
+    public static void main(String[] args) {
+        Program.main(args);
+    }
+}
--- projects/porkmirror/src/main/scala/com/jkbff/ao/playerinfoscraper/Program.scala
+++ projects/porkmirror/src/main/scala/com/jkbff/ao/playerinfoscraper/Program.scala
@@ -28,45 +28,45 @@ object Program {
 		// initialize the log4j component
 		PropertyConfigurator.configure("log4j.xml")
 		
-		Program.run
-	}
-	
-	def run = {
 		try {
-			val startTime = System.currentTimeMillis
-			
-			val orgNameUrl = "http://people.anarchy-online.com/people/lookup/orgs.html?l=%s"
-			
-			//val letters = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "others")
-			val letters = List("m")
-	
-			var orgInfoList = List[OrgInfo]()
-			letters.foreach(letter => {
-				grabPage(orgNameUrl.format(letter)) match {
-					case Some(page) => orgInfoList = pullOrgInfoFromPage(page) ::: orgInfoList
-					case None => log.error("Could not load info for letter: " + letter)
-				}
-			})
-			
-			var numCharacters = new AtomicInteger(0)
-			orgInfoList.par.foreach(orgInfo => {
-				val orgInfoOption = retrieveOrgRoster(orgInfo)
-				if (orgInfoOption.isDefined) {
-					numCharacters.addAndGet(orgInfoOption.get.size)
-					save(orgInfo, orgInfoOption.get, startTime)
-				}
-			})
-			
-			orgInfoList.par.foreach(orgInfo => {
-				updateRemovedGuildMembers(orgInfo, startTime)
-			})
-			
-			log.info("Elapsed time: " + ((System.currentTimeMillis - startTime.toDouble) / 1000) + "s")
-			log.info("Orgs parsed: " + orgInfoList.size)
-			log.info("Characters parsed: " + numCharacters)
+			Program.run
 		} catch {
 			case e => log.error("Could not finish retrieving info", e)
 		}
+	}
+	
+	def run = {
+		val startTime = System.currentTimeMillis
+		
+		val orgNameUrl = "http://people.anarchy-online.com/people/lookup/orgs.html?l=%s"
+		
+		val letters = List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "others")
+		//val letters = List("m")
+
+		var orgInfoList = List[OrgInfo]()
+		letters.foreach(letter => {
+			grabPage(orgNameUrl.format(letter)) match {
+				case Some(page) => orgInfoList = pullOrgInfoFromPage(page) ::: orgInfoList
+				case None => log.error("Could not load info for letter: " + letter)
+			}
+		})
+
+		var numCharacters = new AtomicInteger(0)
+		orgInfoList.par.foreach(orgInfo => {
+			val orgInfoOption = retrieveOrgRoster(orgInfo)
+			if (orgInfoOption.isDefined) {
+				numCharacters.addAndGet(orgInfoOption.get.size)
+				save(orgInfo, orgInfoOption.get, startTime)
+			}
+		})
+		
+		orgInfoList.par.foreach(orgInfo => {
+			updateRemovedGuildMembers(orgInfo, startTime)
+		})
+		
+		log.info("Elapsed time: " + ((System.currentTimeMillis - startTime.toDouble) / 1000) + "s")
+		log.info("Orgs parsed: " + orgInfoList.size)
+		log.info("Characters parsed: " + numCharacters)
 	}
 	
 	def updateRemovedGuildMembers(orgInfo: OrgInfo, time: Long) {
@@ -105,7 +105,8 @@ object Program {
 		grabPage(orgRosterUrl.format(orgInfo.server, orgInfo.guildId)) match {
 			case Some(page: String) => {
 				try {
-					val xml = XML.loadString(page)
+					// remove unicode characters from guild: Otto,4556801,1
+					val xml = XML.loadString(page.replace("\u0010", "").replace("\u0018", ""))
 					orgInfo.faction = (xml \ "side").text
 					
 					val characters = pullCharInfo((xml \\ "member").reverseIterator, orgInfo.guildId, orgInfo.guildName, orgInfo.server)
@@ -115,7 +116,7 @@ object Program {
 					case e: SAXParseException => log.error("Could not parse roster for org: " + orgInfo, e)
 				}
 			}
-			case None => log.error("Could not load info for org: " + orgInfo)
+			case None => log.error("Could not retrieve xml file for org: " + orgInfo)
 		}
 		return None
 	}
@@ -130,7 +131,7 @@ object Program {
 	}
 	
 	def pullOrgInfoFromPage(page: String) = {
-		log.info("Processing page: " + page)
+		log.info("Processing page...")
 		val pattern = """(?s)<a href="http://people.anarchy-online.com/org/stats/d/(\d)/name/(\d+)">(.+?)</a>""".r
 		var orgInfoList: List[OrgInfo] = List[OrgInfo]()
 		
