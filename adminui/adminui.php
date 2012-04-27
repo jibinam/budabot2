c411422a80dfd73b818e3eb8ba4b54b738f77973
joyer83 <joyer83@gmail.com>
2012-04-27 19:55:09 +0000

    - PHP-GTK extension now uses GTK+ 2.24 definitions instead 2.6. This enables among other things GtkBuilder-class which can be used to load newer GtkBuilder based glade-files. - Added Cairo extension as it is now required by the GTK-PHP extension. - Removed old libglade2 extension as it is not needed anymore. - Added glade-file which defines UI for bot's output window. - Updated adminui.php to use the glade-file.

--- adminui/adminui.php
+++ adminui/adminui.php
@@ -1,13 +1,20 @@
 <?php
- 
-$window = new GtkWindow();
-$window->set_title('Hello world');
-$window->connect_simple('destroy', array('gtk', 'main_quit'));
- 
-$widget = new GtkLabel("Hello Budabot!'");
-$widget->set_usize(200, 100);
-$window->add($widget);
- 
-$window->show_all();
-Gtk::main();
 
+// load botwindow.glade file
+$botWindowBuilder = new GtkBuilder();
+$botWindowBuilder->add_from_file(dirname(__FILE__) . '/botwindow.glade');
+
+// get some widgets and objects for easier access
+$botWindow  = $botWindowBuilder->get_object('botwindow');
+$outputView = $botWindowBuilder->get_object('outputView');
+$outputModel = $outputView->get_buffer();
+
+// clicking window's x-button quits the main event loop
+$botWindow->connect_simple('destroy', array('gtk', 'main_quit'));
+
+// set some example text to our bot's output view
+$outputModel->set_text('Bot stdout goes here');
+
+// show bot window and start main event loop
+$botWindow->show_all();
+Gtk::main();
