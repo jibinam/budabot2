46515d2066ac29e959260b7b3da6bef4bd32d304
joyer83 <joyer83@gmail.com>
2012-05-12 10:45:30 +0000

    - Added BotModel implementation which contains list of available bots.  - Control panel's list view now uses BotModel as its data source.  - Implemented SettingModel to load and save settings to conf/adminui_settings.conf. The configuration file is also validated against settings.xsd schema.

--- adminui/Bot.class.php
+++ adminui/Bot.class.php
@@ -4,11 +4,19 @@ require_once 'Process.class.php';
 require_once 'budapi/Budapi.php';
 
 class Bot {
-	
-	public function __construct($settings) {
+
+	private $name;
+	private $settingModel;
+
+	public function __construct($name, $settingModel) {
+		$this->name = $name;
+		$this->settingModel = $settingModel;
 		$this->api = new Budapi();
 		$this->process = new Process();
-		$this->settings = $settings;
+	}
+	
+	public function getName() {
+		return $this->name;
 	}
 	
 	public function start() {
