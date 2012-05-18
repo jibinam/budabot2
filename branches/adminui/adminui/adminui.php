71c0c387fd318bab054e63f2ac67fa81b371da04
joyer83@gmail.com <joyer83@gmail.com@7573f3da-27b8-11df-a76c-adfa62dee608>
2012-05-18 16:06:44 +0000

    - Added missing zlib1.dll. - Theme is now loaded only on Windows platform.

--- branches/adminui/adminui/adminui.php
+++ branches/adminui/adminui/adminui.php
@@ -1,8 +1,8 @@
 <?php
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') { 
 	putenv('GTK_PATH=win32');
+	Gtk::rc_parse('adminui/themes/Cillop-Midnite/gtk-2.0/gtkrc');
 }
-Gtk::rc_parse('adminui/themes/Cillop-Midnite/gtk-2.0/gtkrc');
 
 require_once 'Application.class.php';
 
