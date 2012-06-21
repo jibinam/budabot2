58c1dddb3ed63b1166738b8492def15704416cf9
joyer83@gmail.com <joyer83@gmail.com@7573f3da-27b8-11df-a76c-adfa62dee608>
2012-06-21 19:52:17 +0000

    Separated pages to their own module file.

--- projects/botmanager/addbotwizard.py
+++ projects/botmanager/addbotwizard.py
@@ -1,16 +1,9 @@
 ﻿#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os
 import gtk
-import gobject
 from botconfigfile import BotPhpConfigFile
-
-SELECT_ACTION_PAGE_ID = 1
-SELECT_IMPORT_PAGE_ID = 2
-NAME_BOT_PAGE_ID      = 3
-FINISH_PAGE_ID        = 4
-
+from addbotwizardpages import SelectActionPage, SelectImportPage, NameBotPage, FinishPage
 
 class AddBotWizardController:
 	""""""
@@ -28,10 +21,10 @@ class AddBotWizardController:
 		self.assistant.connect('close', self.onCloseClicked)
 		self.assistant.connect('prepare', self.onPreparePage)
 
-		self.selectActionPage = SelectActionPage(SELECT_ACTION_PAGE_ID, self.builder)
-		self.selectImportPage = SelectImportPage(SELECT_IMPORT_PAGE_ID, self.builder, settingModel)
-		self.botNamePage      = NameBotPage(NAME_BOT_PAGE_ID, self.builder)
-		self.finishPage       = FinishPage(FINISH_PAGE_ID, self.builder)
+		self.selectActionPage = SelectActionPage(self.builder)
+		self.selectImportPage = SelectImportPage(self.builder, settingModel)
+		self.botNamePage      = NameBotPage(self.builder)
+		self.finishPage       = FinishPage(self.builder)
 		self.assistant.appendPage(self.selectActionPage)
 		self.assistant.appendPage(self.selectImportPage)
 		self.assistant.appendPage(self.botNamePage)
@@ -64,12 +57,12 @@ class AddBotWizardController:
 		self.hide()
 
 	def onPreparePage(self, caller, pageWidget):
-		if self.botNamePage.widget == pageWidget:
+		if self.botNamePage.getWidget() == pageWidget:
 			# set default name to the bot name page
 			name = '%s @ RK%d' % (self.botConfig.getVar('name'), self.botConfig.getVar('dimension'))
 			self.botNamePage.setBotName(name)
 			
-		elif self.finishPage.widget == pageWidget:
+		elif self.finishPage.getWidget() == pageWidget:
 			values = []
 			values.append(('Name', self.botNamePage.getBotName()))
 			values.append(('Bot software path', self.selectImportPage.getSelectedBotRootPath()))
@@ -85,31 +78,6 @@ class AddBotWizardController:
 			config.load()
 			self.botConfig = config
 
-class BotImportModel(gtk.ListStore):
-	def __init__(self):
-		"""Constructor method."""
-		super(BotImportModel, self).__init__(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
-
-	def load(self, path):
-		"""Loads all config files from given path and adds them to the model."""
-		self.clear()
-		for fileName in os.listdir(path):
-			# ignore the template file
-			if fileName == 'config.template.php':
-				continue
-			try:
-				# try to load the file as a config file
-				filePath = os.path.join(path, fileName)
-				configFile = BotPhpConfigFile(filePath)
-				configFile.load()
-				name = configFile.getVar('name')
-				dimension = 'RK %s' % configFile.getVar('dimension')
-			except (KeyError, IOError):
-				# ignore files which are not valid config files
-				continue
-			# add to the config file to model
-			self.append((fileName, name, dimension))
-
 class Assistant(gtk.Assistant):
 	def __init__(self):
 		super(Assistant, self).__init__()
@@ -120,9 +88,9 @@ class Assistant(gtk.Assistant):
 	def appendPage(self, page):
 		"""Helper method for adding pages to the wizard."""
 		self.pages.append(page)
-		page.index = self.append_page(page.widget)
-		self.set_page_title(page.widget, page.getTitle())
-		self.set_page_type(page.widget, page.getType())
+		page.index = self.append_page(page.getWidget())
+		self.set_page_title(page.getWidget(), page.getTitle())
+		self.set_page_type(page.getWidget(), page.getType())
 		page.connect('notify::complete', self.onPageCompletenessChanged)
 
 	def getNextPage(self, currentPageIndex):
@@ -139,168 +107,9 @@ class Assistant(gtk.Assistant):
 
 	def onPreparePage(self, caller, pageWidget):
 		for page in self.pages:
-			if page.widget == pageWidget:
+			if page.getWidget() == pageWidget:
 				page.prepare()
-				self.set_page_complete(page.widget, page.get_property('complete'))
+				self.set_page_complete(page.getWidget(), page.get_property('complete'))
 
 	def onPageCompletenessChanged(self, page, property):
-		self.set_page_complete(page.widget, page.get_property('complete'))
-
-class Page(gobject.GObject):
-	# custom properties
-	__gproperties__ = {
-		'complete' : (gobject.TYPE_BOOLEAN, 'complete', 'is page complete', False, gobject.PARAM_READWRITE),
-	}
-
-	def __init__(self, id):
-		self.__gobject_init__()
-		self.id = id
-		self.index = -1
-		self.widget = None
-		self.type = gtk.ASSISTANT_PAGE_CONTENT
-		self.title = ''
-		self.completenessFunc = lambda: True
-		self.completenessFuncArgs = []
-		self.nextPageIdFunc = lambda: None
-		self.nextPageIdFuncArgs = []
-
-	def do_get_property(self, property):
-		"""Returns value of given property.
-		This is required to make GTK's properties to work.
-		"""
-		if property.name == 'complete':
-			return self.completenessFunc(*self.completenessFuncArgs)
-
-	def prepare(self):
-		pass
-
-	def getNextPageId(self):
-		return self.nextPageIdFunc(*self.nextPageIdFuncArgs)
-
-	def getTitle(self):
-		return self.title
-
-	def getType(self):
-		return self.type
-
-	def setType(self, type):
-		"""This method sets type of the page."""
-		self.type = type
-
-	def setTitle(self, title):
-		"""This method sets given string as page's title."""
-		self.title = title
-
-	def setNextPageIdFunc(self, function, *args):
-		"""Sets function which should return id of the next page."""
-		self.nextPageIdFunc = function
-		self.nextPageIdFuncArgs = args
-
-	def setCompletenessFunc(self, function, *args):
-		"""Sets function which should return boolean value which indicates if
-		the page is complete or not.
-		"""
-		self.completenessFunc = function
-		self.completenessFuncArgs = args
-
-	def updateCompleteness(self, *args):
-		"""Notifies any connected listeners that the page complete-status might
-		have changed and should be updated.
-		"""
-		self.notify('complete')
-
-# register class so that custom signals will work
-gobject.type_register(Page)
-
-class SelectActionPage(Page):
-
-	def __init__(self, id, builder):
-		super(SelectActionPage, self).__init__(id)
-		self.setType(gtk.ASSISTANT_PAGE_INTRO)
-		self.setTitle('Add or import bot')
-		self.setNextPageIdFunc(self.nextPageId)
-		self.widget = builder.get_object('selectActionPage')
-		self.addBotRadioButton = builder.get_object('addBotRadioButton')
-		self.importBotRadioButton = builder.get_object('importBotRadioButton')
-		self.importBotRadioButton.set_group(self.addBotRadioButton)
-
-	def nextPageId(self):
-		if self.addBotRadioButton.get_property('active'):
-			return None
-		elif self.importBotRadioButton.get_property('active'):
-			return SELECT_IMPORT_PAGE_ID
-		return None
-
-class SelectImportPage(Page):
-	def __init__(self, id, builder, settingModel):
-		super(SelectImportPage, self).__init__(id)
-		self.setTitle('Import existing bot')
-		self.setNextPageIdFunc(lambda: NAME_BOT_PAGE_ID)
-		self.setCompletenessFunc(lambda self: self.getSelectedBotConfFilePath() != None, self)
-		self.widget = builder.get_object('selectImportPage')
-		self.settingModel = settingModel
-		self.modelPath = ''
-		self.dirChooser = builder.get_object('botImportDirChooser')
-		self.dirChooser.connect('current-folder-changed', self.onBotImportDirChoosen)
-		self.dirChooser.set_current_folder(self.settingModel.getDefaultBotRootPath())
-		self.botImportModel = BotImportModel()
-		self.botView = builder.get_object('importBotListView')
-		self.botView.set_model(self.botImportModel)
-		self.botView.get_selection().connect('changed', self.updateCompleteness)
-
-	def getSelectedBotRootPath(self):
-		"""Returns path to the bot software's root folder."""
-		return self.dirChooser.get_filename()
-
-	def getSelectedBotConfFilePath(self):
-		selected = self.botView.get_selection().get_selected()
-		if selected[0] != None and selected[1] != None:
-			filename = selected[0].get(selected[1], 0)[0]
-			return os.path.join(self.modelPath, filename)
-		return None
-
-	def onBotImportDirChoosen(self, caller):
-		"""This signal handler is called when user chooses a directory in import wizard."""
-		self.modelPath = os.path.join(self.dirChooser.get_filename(), 'conf')
-		if os.path.isdir(self.modelPath):
-			self.botImportModel.load(self.modelPath)
-			# save current path to settings for later use if bots were found
-			if len(self.botImportModel) > 0:
-				self.settingModel.setDefaultBotRootPath(self.dirChooser.get_filename())
-				self.settingModel.save()
-		else:
-			self.botImportModel.clear()
-
-class NameBotPage(Page):
-	def __init__(self, id, builder):
-		super(NameBotPage, self).__init__(id)
-		self.setTitle('Bot Name')
-		self.setNextPageIdFunc(lambda: FINISH_PAGE_ID)
-		# page is complete if given bot name is not empty
-		self.setCompletenessFunc(lambda self: len(self.botNameEntry.get_text()) > 0, self)
-		self.widget = builder.get_object('nameBotPage')
-		self.botNameEntry = builder.get_object('botNameEntry')
-		self.botNameEntry.connect('notify::text', self.updateCompleteness)
-
-	def getBotName(self):
-		return self.botNameEntry.get_text()
-
-	def setBotName(self, name):
-		self.botNameEntry.set_text(name)
-
-class FinishPage(Page):
-	def __init__(self, id, builder):
-		super(FinishPage, self).__init__(id)
-		self.setType(gtk.ASSISTANT_PAGE_CONFIRM)
-		self.setTitle('Summary')
-		self.widget = builder.get_object('finishPage')
-		self.summaryLabel = builder.get_object('summaryLabel')
-
-	def setValues(self, values):
-		contents = ''
-		for key, value in values:
-			if key == 'login' or key == 'password' or key == 'DB username' or key == 'DB password':
-				value = len(value) * '**'
-			contents += '<b>' + key + ':</b> ' + str(value) + '\n'
-		self.summaryLabel.set_markup(contents)
-
+		self.set_page_complete(page.getWidget(), page.get_property('complete'))
---
+++ projects/botmanager/addbotwizardpages.py
@@ -0,0 +1,281 @@
+﻿#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This module contains all page classes used in wizard for adding and
+importing bots.
+
+This module also provides following constants which are used to identify
+each page:
+
+  SELECT_ACTION_PAGE_ID - This is the first page where user can select if he is
+                          going add a new bot or import and existing bot.
+  SELECT_IMPORT_PAGE_ID - This is first page in the import functionality where
+                          user can browse for location of the bot.
+  NAME_BOT_PAGE_ID      - In this page user can give the bot a name.
+  FINISH_PAGE_ID        - This is the final page which shows summary of the
+                          bot settings.
+"""
+
+import os
+import gtk
+import gobject
+from botconfigfile import BotPhpConfigFile
+
+SELECT_ACTION_PAGE_ID = 1
+SELECT_IMPORT_PAGE_ID = 2
+NAME_BOT_PAGE_ID      = 3
+FINISH_PAGE_ID        = 4
+
+class Page(gobject.GObject):
+	"""A common base class for each page class.
+	
+	In order to use this class you need to either create an object of this or
+	one the derived classes. You need to provide ID, type, title string, and
+	a widget which is shown inside the Assistant.
+	
+	In addition, the page object must be added to the Assistant with
+	appendPage() before it can be used.
+	"""
+
+	# custom properties
+	__gproperties__ = {
+		'complete' : (gobject.TYPE_BOOLEAN, 'complete', 'is page complete', False, gobject.PARAM_READWRITE),
+	}
+
+	def __init__(self, id):
+		"""Constructor method.
+		
+		The given id value is used to identify the page object by the
+		Assistant to which it is appended.
+		"""
+		self.__gobject_init__()
+		self.id = id
+		self.index = -1
+		self.widget = None
+		self.type = gtk.ASSISTANT_PAGE_CONTENT
+		self.title = ''
+		self.completenessFunc = lambda: True
+		self.completenessFuncArgs = []
+		self.nextPageIdFunc = lambda: None
+		self.nextPageIdFuncArgs = []
+
+	def do_get_property(self, property):
+		"""Returns value of given property.
+		This is required to make GTK's properties to work.
+		"""
+		if property.name == 'complete':
+			return self.completenessFunc(*self.completenessFuncArgs)
+
+	def prepare(self):
+		pass
+
+	def getWidget(self):
+		"""Returns widget object which represents the visible contents
+		of the page.
+		
+		This method is used by the Assistant.
+		"""
+		return self.widget
+
+	def getNextPageId(self):
+		"""Returns ID value of the next page to where the wizard should change
+		when user clicks 'Forward' button.
+		
+		This method is used by the Assistant.
+		
+		Internally this method calls function object set with
+		setNextPageIdFunc() and returns what it returns.
+		"""
+		return self.nextPageIdFunc(*self.nextPageIdFuncArgs)
+
+	def getTitle(self):
+		"""Returns title string of this page which should be shown in the Assistant.
+		
+		This method is used by the Assistant when it needs to draw the
+		title to screen.
+		"""
+		return self.title
+
+	def getType(self):
+		"""This method returns the GTK type of this page.
+		
+		This method is used by the Assistant.
+		"""
+		return self.type
+
+	def setType(self, type):
+		"""This method sets the GTK type of the page.
+		
+		By default the type is gtk.ASSISTANT_PAGE_CONTENT, if some other type
+		is required then is method should be called to set it correctly.
+		
+		List of other possible constants can be found from here:
+		  http://www.pygtk.org/docs/pygtk/gtk-constants.html#gtk-assistant-page-type-constants
+		"""
+		self.type = type
+
+	def setTitle(self, title):
+		"""This method sets given string as page's title.
+		
+		By default the title string is an empty string and any sub classes
+		should set the title explcitly by calling this method.
+		"""
+		self.title = title
+
+	def setNextPageIdFunc(self, function, *args):
+		"""Sets function which should return id of the next page."""
+		self.nextPageIdFunc = function
+		self.nextPageIdFuncArgs = args
+
+	def setCompletenessFunc(self, function, *args):
+		"""Sets function which should return boolean value which indicates if
+		the page is complete or not.
+		"""
+		self.completenessFunc = function
+		self.completenessFuncArgs = args
+
+	def updateCompleteness(self, *args):
+		"""Notifies any connected listeners that the page complete-status might
+		have changed and should be updated.
+		"""
+		self.notify('complete')
+
+# register class so that custom signals will work
+gobject.type_register(Page)
+
+class SelectActionPage(Page):
+	"""This page class lets user select should he add a new bot or import
+	a existing bot.
+	"""
+
+	def __init__(self, builder):
+		"""Constructor method."""
+		super(SelectActionPage, self).__init__(SELECT_ACTION_PAGE_ID)
+		self.setType(gtk.ASSISTANT_PAGE_INTRO)
+		self.setTitle('Add or import bot')
+		self.setNextPageIdFunc(self.nextPageId)
+		self.widget = builder.get_object('selectActionPage')
+		self.addBotRadioButton = builder.get_object('addBotRadioButton')
+		self.importBotRadioButton = builder.get_object('importBotRadioButton')
+		self.importBotRadioButton.set_group(self.addBotRadioButton)
+
+	def nextPageId(self):
+		"""Returns ID of the next page to where wizard should change."""
+		if self.addBotRadioButton.get_property('active'):
+			return None
+		elif self.importBotRadioButton.get_property('active'):
+			return SELECT_IMPORT_PAGE_ID
+		return None
+
+class SelectImportPage(Page):
+	"""This page class lets users browse for location of the Budabot
+	installation and config file which should be imported to Bot Manager.
+	"""
+	
+	def __init__(self, builder, settingModel):
+		"""Constructor method."""
+		super(SelectImportPage, self).__init__(SELECT_IMPORT_PAGE_ID)
+		self.setTitle('Import existing bot')
+		self.setNextPageIdFunc(lambda: NAME_BOT_PAGE_ID)
+		self.setCompletenessFunc(lambda self: self.getSelectedBotConfFilePath() != None, self)
+		self.widget = builder.get_object('selectImportPage')
+		self.settingModel = settingModel
+		self.modelPath = ''
+		self.dirChooser = builder.get_object('botImportDirChooser')
+		self.dirChooser.connect('current-folder-changed', self.onBotImportDirChoosen)
+		self.dirChooser.set_current_folder(self.settingModel.getDefaultBotRootPath())
+		self.botImportModel = BotImportModel()
+		self.botView = builder.get_object('importBotListView')
+		self.botView.set_model(self.botImportModel)
+		self.botView.get_selection().connect('changed', self.updateCompleteness)
+
+	def getSelectedBotRootPath(self):
+		"""Returns path to the bot software's root folder."""
+		return self.dirChooser.get_filename()
+
+	def getSelectedBotConfFilePath(self):
+		"""Returns path to the currently selected configuration file."""
+		selected = self.botView.get_selection().get_selected()
+		if selected[0] != None and selected[1] != None:
+			filename = selected[0].get(selected[1], 0)[0]
+			return os.path.join(self.modelPath, filename)
+		return None
+
+	def onBotImportDirChoosen(self, caller):
+		"""This signal handler is called when user chooses a directory in import wizard."""
+		self.modelPath = os.path.join(self.dirChooser.get_filename(), 'conf')
+		if os.path.isdir(self.modelPath):
+			self.botImportModel.load(self.modelPath)
+			# save current path to settings for later use if bots were found
+			if len(self.botImportModel) > 0:
+				self.settingModel.setDefaultBotRootPath(self.dirChooser.get_filename())
+				self.settingModel.save()
+		else:
+			self.botImportModel.clear()
+
+class BotImportModel(gtk.ListStore):
+	def __init__(self):
+		"""Constructor method."""
+		super(BotImportModel, self).__init__(gobject.TYPE_STRING, gobject.TYPE_STRING, gobject.TYPE_STRING)
+
+	def load(self, path):
+		"""Loads all config files from given path and adds them to the model."""
+		self.clear()
+		for fileName in os.listdir(path):
+			# ignore the template file
+			if fileName == 'config.template.php':
+				continue
+			try:
+				# try to load the file as a config file
+				filePath = os.path.join(path, fileName)
+				configFile = BotPhpConfigFile(filePath)
+				configFile.load()
+				name = configFile.getVar('name')
+				dimension = 'RK %s' % configFile.getVar('dimension')
+			except (KeyError, IOError):
+				# ignore files which are not valid config files
+				continue
+			# add to the config file to model
+			self.append((fileName, name, dimension))
+
+class NameBotPage(Page):
+	"""This page class lets user to give a name for the bot."""
+
+	def __init__(self, builder):
+		"""Constructor method."""
+		super(NameBotPage, self).__init__(NAME_BOT_PAGE_ID)
+		self.setTitle('Bot Name')
+		self.setNextPageIdFunc(lambda: FINISH_PAGE_ID)
+		# page is complete if given bot name is not empty
+		self.setCompletenessFunc(lambda self: len(self.botNameEntry.get_text()) > 0, self)
+		self.widget = builder.get_object('nameBotPage')
+		self.botNameEntry = builder.get_object('botNameEntry')
+		self.botNameEntry.connect('notify::text', self.updateCompleteness)
+
+	def getBotName(self):
+		return self.botNameEntry.get_text()
+
+	def setBotName(self, name):
+		self.botNameEntry.set_text(name)
+
+class FinishPage(Page):
+	"""The final page in the wizard. Lists a summary of the bot settings
+	before it is added to the Bot Manager.
+	"""
+
+	def __init__(self, builder):
+		"""Constructor method."""
+		super(FinishPage, self).__init__(FINISH_PAGE_ID)
+		self.setType(gtk.ASSISTANT_PAGE_CONFIRM)
+		self.setTitle('Summary')
+		self.widget = builder.get_object('finishPage')
+		self.summaryLabel = builder.get_object('summaryLabel')
+
+	def setValues(self, values):
+		"""Sets a list of key-value tuples to be shown in the page."""
+		contents = ''
+		for key, value in values:
+			if key == 'login' or key == 'password' or key == 'DB username' or key == 'DB password':
+				value = len(value) * '**'
+			contents += '<b>' + key + ':</b> ' + str(value) + '\n'
+		self.summaryLabel.set_markup(contents)
