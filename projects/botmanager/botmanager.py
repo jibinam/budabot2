fd8669d49936fd1f782ee8fdfcb16ae6ebe7b782
joyer83@gmail.com <joyer83@gmail.com@7573f3da-27b8-11df-a76c-adfa62dee608>
2012-07-20 08:11:52 +0000

    Now you can run only one instance of the bot manager. Requires tendo-egg.

--- projects/botmanager/botmanager.py
+++ projects/botmanager/botmanager.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# make sure that there is only one instance of this process running
+from tendo import singleton
+singletonGuard = singleton.SingleInstance()
+
 from application import Application
 
 # start the application
